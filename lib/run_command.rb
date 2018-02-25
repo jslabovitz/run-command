bc90da89cc489229b4e79b12638d1e139a3fb294
John Labovitz <johnl@johnlabovitz.com>
2018-02-25 11:29:57 -0600

    Rename method.

--- lib/run_command.rb
+++ lib/run_command.rb
@@ -4,7 +4,7 @@ module RunCommand
 
   class Error < Exception; end
 
-  def self.run(command, *args)
+  def self.run_command(command, *args)
     options = args.last.kind_of?(Hash) ? args.pop : {}
     env = options.delete(:env) || {}
     verbose = options.delete(:verbose) || false
@@ -29,10 +29,10 @@ end
 
 if $0 == __FILE__
 
-  RunCommand.run('echo', %w{a b}, verbose: true)
-  RunCommand.run('echo', %w{a b}, verbose: true, env: { 'FOO' => 'bar' })
-  RunCommand.run('echo', %w{a b}, verbose: true, env: { 'FOO' => 'bar' }, argv0: 'DATE')
-  RunCommand.run('echo', %w{a b}, verbose: true, pretend: true)
-  RunCommand.run('echo', ['a', 'b', [1, :foo]], verbose: true)
+  RunCommand.run_command('echo', %w{a b}, verbose: true)
+  RunCommand.run_command('echo', %w{a b}, verbose: true, env: { 'FOO' => 'bar' })
+  RunCommand.run_command('echo', %w{a b}, verbose: true, env: { 'FOO' => 'bar' }, argv0: 'DATE')
+  RunCommand.run_command('echo', %w{a b}, verbose: true, pretend: true)
+  RunCommand.run_command('echo', ['a', 'b', [1, :foo]], verbose: true)
 
 end
