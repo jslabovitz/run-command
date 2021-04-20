26e3775546d7641f1a61cdbd734b7deed3e75b70
John Labovitz <johnl@johnlabovitz.com>
2021-04-20 09:42:18 -0400

    Rewrite to allow STDIN input, and to always raise an exception on non-zero exit status.

--- lib/run-command.rb
+++ lib/run-command.rb
@@ -2,15 +2,16 @@ require 'shellwords'
 
 module RunCommand
 
-  def run_command(command, *args)
-    command = command.to_s
-    options = args.last.kind_of?(Hash) ? args.pop.dup : {}
-    options[:exception] = true
+  class CommandFailed < Exception; end
+
+  def run_command(*args, **options)
+    command = args.shift
     args = args.flatten.compact.map(&:to_s)
     env = options.delete(:env) || {}
     verbose = options.delete(:verbose) || false
     pretend = options.delete(:pretend) || false
     argv0 = options.delete(:argv0)
+    input = options.delete(:input)
     if verbose
       warn "$ %s%s" % [
         env.empty? ? nil : env.map { |kv| kv.join('=') }.join(' ') + ' ',
@@ -18,11 +19,13 @@ module RunCommand
       ]
     end
     unless pretend
-      data = ''
-      IO.popen(env, [argv0 ? [command, argv0] : command, *args], 'r', options) do |io|
-        data += io.read
+      output = IO.popen(env, [argv0 ? [command, argv0] : command, *args], 'r+', options) do |io|
+        io.write(input.to_s) if input
+        io.close_write
+        io.read
       end
-      data
+      raise CommandFailed, "Command #{command.to_s.inspect} failed: #{$?}" if $? != 0
+      output
     end
   end
 
